eaf7323d3b5f52f5df3a0f6e87b8d139faa5096f
DavidBachmann <dabbi@dabbi.is>
2025-01-21 16:46:23 +0000

    customize completion sources

---
+++ lua/plugins/cmp.lua
@@ -0,0 +1,26 @@
+return {
+  "hrsh7th/nvim-cmp",
+  opts = function(_, opts)
+    local cmp = require "cmp"
+    opts.sources = {
+      {
+        name = "nvim_lsp",
+        priority = 1000,
+        entry_filter = function(entry)
+          -- sources to filter out
+          local filtered_sources = {
+            ["nvim_lsp:emmet_ls"] = true,
+            ["nvim_lsp:vtsls"] = true,
+          }
+
+          local source_name = entry.source:get_debug_name()
+          require "notify"(source_name)
+
+          -- Return false if the source is in our filtered table
+          return not filtered_sources[source_name]
+        end,
+      },
+      { name = "path", priority = 250 },
+    }
+  end,
+}
--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -3,12 +3,10 @@ return {
   -- use mason-lspconfig to configure LSP installations
   {
     "williamboman/mason-lspconfig.nvim",
-    -- overrides `require("mason-lspconfig").setup(...)`
     opts = {
       ensure_installed = {
         "lua_ls",
-        -- "ts_ls",
-        -- add more arguments for adding more language servers
+        "ts_ls",
       },
     },
   },
@@ -20,18 +18,7 @@ return {
       ensure_installed = {
         "stylua",
         "prettier",
-        "eslint",
-        -- add more arguments for adding more null-ls sources
-      },
-    },
-  },
-  {
-    "jay-babu/mason-nvim-dap.nvim",
-    -- overrides `require("mason-nvim-dap").setup(...)`
-    opts = {
-      ensure_installed = {
-        "python",
-        -- add more arguments for adding more debuggers
+        -- "eslint",
       },
     },
   },
